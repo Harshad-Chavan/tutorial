0996c303a117ce47484803459fdeb078d1be28e4
Harshad-desktop <harshadchavan141@gmail.com>
2020-07-07 22:04:51 +0530

    Refactor using mixins

--- snippets/views.py
+++ snippets/views.py
@@ -8,6 +8,10 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+#Imports for using mixins
+from rest_framework import mixins
+from rest_framework import generics
+
 
 # @csrf_exempt
 # def snippet_list(request):
@@ -22,23 +26,6 @@ from rest_framework.views import APIView
 #             serializer.save
 #             return JsonResponse(serializer.data, status=201)
 #         return JsonResponse(serializer.errors, status=400)
-
-#refactor one using apiviews and status and response object
-# @api_view(['GET', 'POST'])
-# def snippet_list(request,format=None):
-#     if request.method == "GET":
-#         snippets = Snippet.objects.all()
-#         serializer = SnippetSerializer(snippets,many=True)
-#         return Response(serializer.data)
-#     elif request.method == "POST":
-#         # data = JSONParser().parse(request)
-#         print(request.data)
-#         serializer = SnippetSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 # @csrf_exempt
 # def snippet_detail(request,pk):
@@ -62,6 +49,23 @@ from rest_framework.views import APIView
 #     elif request.method == 'DELETE':
 #         snippet.delete()
 #         return HttpResponse(status=204)
+
+
+#Refactor one using apiviews and status and response object
+# @api_view(['GET', 'POST'])
+# def snippet_list(request,format=None):
+#     if request.method == "GET":
+#         snippets = Snippet.objects.all()
+#         serializer = SnippetSerializer(snippets,many=True)
+#         return Response(serializer.data)
+#     elif request.method == "POST":
+#         # data = JSONParser().parse(request)
+#         print(request.data)
+#         serializer = SnippetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # @api_view(['GET', 'PUT','DELETE'])
 # def snippet_detail(request,pk,format=None):
@@ -88,40 +92,68 @@ from rest_framework.views import APIView
 
 
 # REFACTOR 2 CHANGING INTO CLASS BASED VIEWS
-class SnippetList(APIView):
+# class SnippetList(APIView):
 
-    def get(self,request,format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets,many=True)
-        return Response(serializer.data)
+#     def get(self,request,format=None):
+#         snippets = Snippet.objects.all()
+#         serializer = SnippetSerializer(snippets,many=True)
+#         return Response(serializer.data)
 
         
-    def post(self,request,format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serialzer.is_valid():
-            serializer.save
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+#     def post(self,request,format=None):
+#         serializer = SnippetSerializer(data=request.data)
+#         if serialzer.is_valid():
+#             serializer.save
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
 
 
-class SnippetDetail(APIView):
-    def get_object(self,pk):
-        try:
-            snippet = Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+# class SnippetDetail(APIView):
+#     def get_object(self,pk):
+#         try:
+#             snippet = Snippet.objects.get(pk=pk)
+#         except Snippet.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = SnippetSerializer(snippet, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-    def delete(self,request,pk):
-        snippet = self.get_object(pk)
-        snippet.delete
-        return Response(status=status.HTTP_204_NO_CONTENT)    
+#     def delete(self,request,pk):
+#         snippet = self.get_object(pk)
+#         snippet.delete
+#         return Response(status=status.HTTP_204_NO_CONTENT)    
+
+#REFACTOR 3: USING MIXINS
+class SnippetList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class SnippetDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
